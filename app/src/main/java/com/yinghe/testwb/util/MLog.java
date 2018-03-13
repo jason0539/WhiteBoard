41df6b6b16af3748d974e23744ffb4ac77a6d7a1
jason0539 <jason0539@163.com>
2018-03-13 11:27:34 +0800

    日志工具

---
+++ app/src/main/java/com/yinghe/testwb/util/MLog.java
@@ -0,0 +1,39 @@
+package com.yinghe.testwb.util;
+
+import android.util.Log;
+
+/**
+ * Created by liuzhenhui on 2018/3/13.
+ */
+
+public class MLog {
+    public static final String TAG = "LZH_SKETCH";
+
+    private static boolean DEBUG = true;
+
+    public static final String TAG_SOCKET = "TAG_SOCKET";
+    public static final String TAG_SKETCH = "TAG_SKETCH";
+
+    private static boolean isDebug() {
+        return DEBUG;
+    }
+
+    public static final void d(String subTag, String msg) {
+        if (isDebug()) {
+            Log.d(TAG, subTag + "-->" + msg);
+        }
+    }
+
+    public static final void e(String subTag, String msg) {
+        if (isDebug()) {
+            Log.e(TAG, subTag + "-->" + msg);
+        }
+    }
+
+    public static final void v(String subTag, String msg) {
+        if (isDebug()) {
+            Log.v(TAG, subTag + "-->" + msg);
+        }
+    }
+
+}
