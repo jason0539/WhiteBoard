c105f496066872a4db867d0ad4fc4a3a68236444
jason0539 <jason0539@163.com>
2018-03-26 17:29:49 +0800

    注释

--- whiteboardlib/src/main/java/com/lzh/whiteboardlib/WhiteBoardData.java
+++ whiteboardlib/src/main/java/com/lzh/whiteboardlib/WhiteBoardData.java
@@ -7,13 +7,13 @@ package com.lzh.whiteboardlib;
 
 public class WhiteBoardData {
 
-    //坐标数组，用坐标内用逗号隔开,坐标间用分号隔开,例如:1,2;2,3
+    //坐标数组：用坐标内用逗号隔开,坐标间用分号隔开,例如:1,2;2,3
     String path;
-    //线条宽度
+    //线条宽度：px
     float width;
-    //线条颜色，16进制
+    //线条颜色：16进制
     String color;
-    //线段类型,0曲线，1圆，2线段，3矩形
+    //线段类型：0曲线，1圆，2线段，3矩形，4橡皮擦
     int type;
 
     public String getPath() {
